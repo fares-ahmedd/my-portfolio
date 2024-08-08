9fb46a507c9f223336908f8e89807a57c2318aeb
Fares Ahmed <fares.haliim@gmail.com>
2024-08-08 14:46:00 +0300

    add some preview icons to home component  ⤴️⤴️🎗️

--- src/components/home/AnimationComputer.tsx
+++ src/components/home/AnimationComputer.tsx
@@ -1,8 +1,31 @@
 import Lottie from "lottie-react";
 import computer from "../../../public/computer-animation.json";
+import { FaHtml5, FaJsSquare } from "react-icons/fa";
 
 function AnimationComputer() {
-  return <Lottie animationData={computer} loop={true} autoplay={true}></Lottie>;
+  return (
+    <div className="relative">
+      <Lottie animationData={computer} loop={true} autoplay={true}></Lottie>
+      <div
+        className="absolute top-3 right-3  bg-third-background p-2 rounded-full duration-300 hover:bg-main-background icon-shadow hover:shadow-none hover:animate-pulse"
+        title="HTML"
+      >
+        <FaHtml5 color="#e34c26" className="text-2xl " />
+      </div>
+      <div
+        className="absolute bottom-12 right-3  bg-third-background p-2 rounded-full duration-300 hover:bg-main-background icon-shadow hover:shadow-none hover:animate-pulse"
+        title="CSS"
+      >
+        <FaHtml5 color="#2965f1" className="text-3xl " />
+      </div>{" "}
+      <div
+        className="absolute bottom-3 left-3  bg-third-background p-2 rounded-full duration-300 hover:bg-main-background icon-shadow hover:shadow-none hover:animate-pulse"
+        title="Javascript"
+      >
+        <FaJsSquare color="#c2ad10" className="text-4xl " />
+      </div>
+    </div>
+  );
 }
 
 export default AnimationComputer;
--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -3,9 +3,14 @@ import AnimationComputer from "./AnimationComputer";
 
 function Home() {
   return (
-    <section className="min-h-[calc(100vh-82px)] grid grid-cols-1 md:grid-cols-2 items-center gap-2">
+    <section className="min-h-[calc(100vh-82px)] grid grid-cols-1 md:grid-cols-2 items-center gap-2 relative">
       <AboutMe />
       <AnimationComputer />
+      <img
+        src="/dots.png"
+        alt="dots"
+        className="absolute bottom-0 right-0 opacity-70 h-[120px] max-md:hidden"
+      />
     </section>
   );
 }
